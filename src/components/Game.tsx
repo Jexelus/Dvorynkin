8da5d09810d877dfad072777604600fe5b3dd8e6
Jexelus <danil.smi@inbox.ru>
2023-12-21 23:57:42 +0300

    INIT COMMIT

---
+++ src/components/Game.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import {SafeAreaView, StyleSheet} from "react-native";
+import {Colors} from "../styles/colors";
+
+export default function snake():JSX.Element{
+    return (
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+}
+const style = StyleSheet.create({
+    container:{
+        backgroundColor: Colors.primary
+    }
+})
